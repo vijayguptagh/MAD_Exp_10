f71e798d1d8876234098d59fb569ccf71fa60ad3
vijayguptagh <120735063+vijayguptagh@users.noreply.github.com>
2024-04-25 03:17:49 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercommerce/screens/product.dart';
+
+import 'screens/login.dart';
+
+void main() => runApp(EcommerceApp());
+
+class EcommerceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        dividerColor: Color(0xFFECEDF1),
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        primaryColor: Color(0xFFF93963),
+        accentColor: Colors.cyan[600],
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          headline3: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          subtitle1: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+          subtitle2: TextStyle(fontSize: 16),
+          bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Montserrat'),
+          headline4: TextStyle(
+              fontSize: 14.0, fontFamily: 'Montserrat1', color: Colors.white),
+          headline5: TextStyle(
+              fontSize: 14.0, fontFamily: 'Montserrat', color: Colors.black54),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Ecommerce UI Kit',
+      home: LoginScreen(),
+      routes: {
+        '/product': (context) => ProductPage(),
+      },
+    );
+  }
+}
+
+class TabLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      color: Colors.yellow,
+      home: DefaultTabController(
+        length: 4,
+        child: new Scaffold(
+          body: TabBarView(
+            children: [
+              new Container(
+                color: Colors.yellow,
+              ),
+              new Container(
+                color: Colors.orange,
+              ),
+              new Container(
+                color: Colors.lightGreen,
+              ),
+              new Container(
+                color: Colors.red,
+              ),
+            ],
+          ),
+          bottomNavigationBar: new TabBar(
+            tabs: [
+              Tab(
+                icon: new Icon(Icons.home),
+              ),
+              Tab(
+                icon: new Icon(Icons.rss_feed),
+              ),
+              Tab(
+                icon: new Icon(Icons.perm_identity),
+              ),
+              Tab(
+                icon: new Icon(Icons.settings),
+              )
+            ],
+            labelColor: Colors.yellow,
+            unselectedLabelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.red,
+          ),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
